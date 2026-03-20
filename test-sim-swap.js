6f464b085d05fef19fffcfe3251d937384b6fd6f
ahmeddu93391 <ahmeddu93391@gmail.com>
2026-03-20 14:06:51 +0100

    Create test-sim-swap.js

---
+++ test-sim-swap.js
@@ -0,0 +1,17 @@
+const axios = require('axios');
+const BASE = 'http://localhost:3000';
+
+async function main() {
+  const r = await axios.post(`${BASE}/oauth/token`,
+    'grant_type=client_credentials&client_id=test&client_secret=test',
+    { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } }
+  );
+  const headers = { Authorization: `Bearer ${r.data.access_token}`, 'Content-Type': 'application/json' };
+
+  const res = await axios.post(`${BASE}/sim-swap/v0/check`,
+    { phoneNumber: '0900000000', maxAge: 24 }, { headers }
+  );
+  console.log('SIM Swap :', res.data);
+}
+
+main().catch(e => console.error('Erreur :', e.response ? e.response.data : e.message));
